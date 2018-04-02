59a0dd717f9da4418e00ed0bf9596065e1820409
daheige <daheige@users.noreply.github.com>
2018-04-02 17:59:34 +0800

    Update helper.php

--- application/common/helper.php
+++ application/common/helper.php
@@ -105,7 +105,7 @@ if (!function_exists('get_config')) {
         if (strpos($name, '.') !== false) {
             $arr = explode('.', $name);
             //优先从环境目录读取,最后从Conf目录下读取
-            $filename = CONF_PATH . env_str() . '/' . $arr[0] . '.php';
+            $filename = CONF_PATH . '/' . env_str() . '/' . $arr[0] . '.php';
             if (!is_file($filename)) {
                 $filename = CONF_PATH . '/' . $arr[0] . '.php';
                 if (!is_file($filename)) {
@@ -135,7 +135,7 @@ if (!function_exists('get_config')) {
 
         //读取整个文件内容
         //优先从环境目录读取,最后从Conf目录下读取
-        $filename = CONF_PATH . env_str() . '/' . $name . '.php';
+        $filename = CONF_PATH . '/' . env_str() . '/' . $name . '.php';
         if (!is_file($filename)) {
             $filename = CONF_PATH . '/' . $name . '.php';
             if (!is_file($filename)) {
